d7bcaad83a919cba02373fa4919766345d9b98a3
Rowan Dempster <rowandempster@v1020-wn-113-24.campus-dynamic.uwaterloo.ca>
2017-05-06 09:25:35 -0400

    more log

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.listen(app.get('port'), function() {
 
 
 app.post('/webhook/', function (req, res) {
-    console.log("webhook got something: " + req);
+    console.log("webhook got something: " + req.postback);
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
