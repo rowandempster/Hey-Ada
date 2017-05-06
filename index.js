2118d6678ce292e57a03e173d07c2029126614aa
Rowan Dempster <rowandempster@v1020-wn-113-24.campus-dynamic.uwaterloo.ca>
2017-05-06 09:38:32 -0400

    fix?

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.listen(app.get('port'), function() {
 
 
 app.post('/webhook/', function (req, res) {
-    console.log(util.inspect(req.body.entry[0].messaging.postback, false, null))
+    console.log(util.inspect(req.body.entry[0].messaging[0].postback, false, null))
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
