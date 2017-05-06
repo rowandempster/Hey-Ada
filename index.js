5f368414fd15ecf5fc5d73ed4a02c581e7c09c45
Jacky Zhu <jacky.zhu97@gmail.com>
2017-05-06 17:42:54 -0400

    getbackresult

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ app.post('/webhook/', function (req, res) {
         sendTextMessage(sender, "Thank you for offering support, you will receive a notification when you need help.");
         addToSupports(sender);
       }
+      broadcastTextToGroupIfGroupExists(sender, text); 
       sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
     }
   }
@@ -209,5 +210,16 @@ function saveGroup(supporterArray, requesterId){
   groupCreated.save(function(err){});
 }
 
+// supporters.find({group that constais id}, callback)
+function broadcastTextToGroupIfGroupExists(senderid, text) {
+  var callbackqueryresult = function (result) {
+    result.forEach(function (groupmember) {
+     sendTextMessage(groupmember.id, text)
+    })
+    console.log("Logging result", result);
+  }
+  console.log("Making a request to the database")
+  Group.find({ "members": { $elemMatch: {"id" : senderid}} }, callbackqueryresult);
+}
 
 const token = "EAAWV1QbgKMMBACBKsgZCPgdK9F3tN03SynQrdLybpRz5OrSVZB7Rvxf9frZCxJZBS6X2ViUBtu0jUQWeAE0DPQYYnQX16Xwakyo36hO0MPZBkOuiPCAZCnHJ5hdzlkZAd7PcFDsZBLw0J33NL6d8uQZA0ZBqUVd5OZA5TFyIhiHFEYJqz1gcs2yqRnS"
